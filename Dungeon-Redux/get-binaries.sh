76ae070552d76420c86ae6cc23c39c7385c75ced
Andy Mahoney <32443674+BoneKing@users.noreply.github.com>
2021-01-21 12:13:28 -0500

    get binaries, needs a git clone of Dungeon-Redux at home

---
+++ Dungeon-Redux/get-binaries.sh
@@ -0,0 +1,3 @@
+cp ~/Dungeon-Redux/bin/Release/netcoreapp3.1/win10-x64.zip /var/www/html/Dungeon-Redux/
+cp ~/Dungeon-Redux/bin/Release/netcoreapp3.1/osx.10.11-x64.zip /var/www/html/Dungeon-Redux/
+cp ~/Dungeon-Redux/bin/Release/netcoreapp3.1/linux-x64.zip /var/www/html/Dungeon-Redux/
