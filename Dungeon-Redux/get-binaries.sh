f9b72923983167b4181d0c0bdd26619d69f701d3
Andy Mahoney <32443674+BoneKing@users.noreply.github.com>
2021-01-21 12:14:16 -0500

    comment

--- Dungeon-Redux/get-binaries.sh
+++ Dungeon-Redux/get-binaries.sh
@@ -1,3 +1,4 @@
+#You need to have a git clone copy of DUngeon-Redux located at ~/ 
 cp ~/Dungeon-Redux/bin/Release/netcoreapp3.1/win10-x64.zip /var/www/html/Dungeon-Redux/
 cp ~/Dungeon-Redux/bin/Release/netcoreapp3.1/osx.10.11-x64.zip /var/www/html/Dungeon-Redux/
 cp ~/Dungeon-Redux/bin/Release/netcoreapp3.1/linux-x64.zip /var/www/html/Dungeon-Redux/
