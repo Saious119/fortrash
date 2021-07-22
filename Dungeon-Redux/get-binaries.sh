a4f45e220efcabc44fecc5b76d6e4822760c3553
BoneKing <mahonea@clarkson.edu>
2021-07-22 04:21:43 +0000

    change name of dungeon_redux zips

--- Dungeon-Redux/get-binaries.sh
+++ Dungeon-Redux/get-binaries.sh
@@ -1,4 +1,4 @@
 #You need to have a git clone copy of DUngeon-Redux located at ~/ 
-cp ~/Dungeon-Redux/bin/Release/netcoreapp3.1/win10-x64.zip /var/www/html/Dungeon-Redux/
-cp ~/Dungeon-Redux/bin/Release/netcoreapp3.1/osx.10.11-x64.zip /var/www/html/Dungeon-Redux/
-cp ~/Dungeon-Redux/bin/Release/netcoreapp3.1/linux-x64.zip /var/www/html/Dungeon-Redux/
+cp ~/Dungeon-Redux/bin/Release/net5.0/win10-x64.zip /var/www/html/Dungeon-Redux/Dungeon-Redux-win10_x64.zip
+cp ~/Dungeon-Redux/bin/Release/net5.0/osx.10.11-x64.zip /var/www/html/Dungeon-Redux/Dungeon-Redux-osx_x64.zip
+cp ~/Dungeon-Redux/bin/Release/net5.0/linux-x64.zip /var/www/html/Dungeon-Redux/Dungeon-Redux-linux_x64.zip
